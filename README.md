8df44f7a204b1853d9d083c1baffde43e318da31
sirajchaulagai <64361176+sirajchaulagai@users.noreply.github.com>
2022-06-02 07:27:41 +0545

    initial phase

---
+++ README.md
@@ -0,0 +1,2 @@
+** Lab_1 Homework
+![image](Screenshot_20220602_072455.png)
---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+}
